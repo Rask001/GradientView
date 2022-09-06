2e30c0b72c5c5fe72f3e82ecbd20931d519f4c43
Anton Prokopev <97259692+Rask001@users.noreply.github.com>
2022-09-07 02:17:35 +0300

    Update GradientView.swift

--- GradientView.swift
+++ GradientView.swift
@@ -74,3 +74,18 @@ class GradientView: UIView {
 		super.init(coder: coder)
 	}
 }
+
+
+
+extension UIView {
+	func applyGradients(cornerRadius: CGFloat) {
+		self.backgroundColor = nil
+		self.layoutIfNeeded()
+		let gradientView = GradientView(from: .topTrailing, to: .bottomLeading, startColor: #colorLiteral(red: 0.8309458494, green: 0.7057176232, blue: 0.9536159635, alpha: 1), endColor: #colorLiteral(red: 0.4032760262, green: 0.7716199756, blue: 1, alpha: 1))
+		if let gradientLayer = gradientView.layer.sublayers?.first as? CAGradientLayer {
+			gradientLayer.frame = self.bounds
+			gradientLayer.cornerRadius = cornerRadius
+		self.layer.insertSublayer(gradientLayer, at: 0)
+		}
+	}
+}
