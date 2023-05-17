b556f5ba1d2043eea5fb1f37a947764e65a1d23a
Anton Prokopev <97259692+Rask001@users.noreply.github.com>
2023-05-17 13:08:46 +0300

    Update GradientView.swift

--- GradientView.swift
+++ GradientView.swift
@@ -1,91 +1,111 @@
-//Universal file for create GradientLayer
-
-
+/*
+ GradientView, нужен для добавления градиента на вью.
+ Имеется 2 публичных метода:
+ applyGradient - метод добавляет градиент на указанную в параметр view.
+ getGradientLayer - метод возвращает градиент слой 'CAGradientLayer' c установленными параметрами.
+ 
+ Created by Антон Прокопьев on 20.12.2022.
+ */
 
 import UIKit
 
-class GradientView: UIView {
-	
-	private let gradientLayer = CAGradientLayer()
-	
-	enum Point {
-		case topLeading
-		case leading
-		case bottomLeading
-		case top
-		case center
-		case bottom
-		case topTrailing
-		case trailing
-		case bottomTrailing
-		
-		var point: CGPoint {
-			switch self {
-			case .topLeading:
-				return CGPoint(x: 0, y: 0)
-			case .leading:
-				return CGPoint(x: 0, y: 0.5)
-			case .bottomLeading:
-				return CGPoint(x: 0, y: 1)
-			case .top:
-				return CGPoint(x: 0.5, y: 0)
-			case .center:
-				return CGPoint(x: 0.5, y: 0.5)
-			case .bottom:
-				return CGPoint(x: 0.5, y: 1)
-			case .topTrailing:
-				return CGPoint(x: 1, y: 0)
-			case .trailing:
-				return CGPoint(x: 1, y: 0.5)
-			case .bottomTrailing:
-				return CGPoint(x: 1, y: 1)
-			}
-		}
-	}
-	
-	init(from: Point, to: Point, startColor: UIColor?, endColor: UIColor?) {
-		self.init()
-		setupGradient(from: from, to: to, startColor: startColor, endColor: endColor)
-	}
-	
-	override init(frame: CGRect) {
-		super.init(frame: frame)
-	}
-	
-	override func layoutSubviews() {
-		super.layoutSubviews()
-		gradientLayer.frame = bounds
-	}
-	
-	private func setupGradient(from: Point, to: Point, startColor: UIColor?, endColor: UIColor?) {
-		self.layer.addSublayer(gradientLayer)
-		setupGradientColors(startColor: startColor, endColor: endColor)
-		gradientLayer.startPoint = from.point
-		gradientLayer.endPoint = to.point
-	}
-	
-	private func setupGradientColors(startColor: UIColor?, endColor: UIColor?) {
-		if let startColor = startColor, let endColor = endColor {
-			gradientLayer.colors = [startColor.cgColor, endColor.cgColor ]
-		}
-	}
-	
-	required init?(coder: NSCoder) {
-		super.init(coder: coder)
-	}
+//MARK: - GradientView
+open class GradientView: UIView {
+    
+    //MARK: - Property
+    private let gradientLayer = CAGradientLayer()
+    
+    //MARK: - Init
+    private init(from: Point, to: Point, startColor: UIColor, endColor: UIColor) {
+        self.init()
+        setupGradient(from: from, to: to, startColor: startColor, endColor: endColor)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
 }
 
+//MARK: - Public methods
+extension GradientView {
+    ///метод добавляет градиент на layer, указанной View
+    public func applyGradient(view: UIView, from: Point, to: Point, startColor: UIColor, endColor: UIColor) {
+        view.backgroundColor = nil
+        view.layoutIfNeeded()
+        let gradientView = GradientView(from: from, to: to, startColor: startColor, endColor: endColor)
+        if let gradientLayer = gradientView.layer.sublayers?.first as? CAGradientLayer {
+            gradientLayer.frame = view.bounds
+            view.layer.insertSublayer(gradientLayer, at: 0)
+        }
+    }
+    
+    /// метод возвращает градиент.
+    /// Не забыть указать - gradient.frame = view.bounds.
+    /// Не забыть добавить: layer.addSublayer(grad).
+    /// Использлвать в методах viewDidLayout || viewDidLayoutSubviews!
+    public func getGradientLayer(from: Point, to: Point, startColor: UIColor, endColor: UIColor) -> CAGradientLayer {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [startColor.cgColor, endColor.cgColor]
+        gradientLayer.startPoint = from.point
+        gradientLayer.endPoint = to.point
+        return gradientLayer
+    }
+}
 
+//MARK: - Private methods
+extension GradientView {
+    private func setupGradient(from: Point, to: Point, startColor: UIColor?, endColor: UIColor?) {
+        self.layer.addSublayer(gradientLayer)
+        setupGradientColors(startColor: startColor, endColor: endColor)
+        gradientLayer.startPoint = from.point
+        gradientLayer.endPoint = to.point
+    }
+    
+    private func setupGradientColors(startColor: UIColor?, endColor: UIColor?) {
+        if let startColor = startColor, let endColor = endColor {
+            gradientLayer.colors = [startColor.cgColor, endColor.cgColor]
+        }
+    }
+}
 
-extension UIView {
-	func applyGradients(cornerRadius: CGFloat) {
-		self.backgroundColor = nil
-		self.layoutIfNeeded()
-		let gradientView = GradientView(from: .topTrailing, to: .bottomLeading, startColor: #colorLiteral(red: 0.8309458494, green: 0.7057176232, blue: 0.9536159635, alpha: 1), endColor: #colorLiteral(red: 0.4032760262, green: 0.7716199756, blue: 1, alpha: 1))
-		if let gradientLayer = gradientView.layer.sublayers?.first as? CAGradientLayer {
-			gradientLayer.frame = self.bounds
-			gradientLayer.cornerRadius = cornerRadius
-		self.layer.insertSublayer(gradientLayer, at: 0)
-		}
-	}
+//MARK: - Point Enum
+extension GradientView {
+    public enum Point {
+        case topLeading
+        case leading
+        case bottomLeading
+        case top
+        case center
+        case bottom
+        case topTrailing
+        case trailing
+        case bottomTrailing
+        
+        var point: CGPoint {
+            switch self {
+            case .topLeading:
+                return CGPoint(x: 0, y: 0)
+            case .leading:
+                return CGPoint(x: 0, y: 0.5)
+            case .bottomLeading:
+                return CGPoint(x: 0, y: 1)
+            case .top:
+                return CGPoint(x: 0.5, y: 0)
+            case .center:
+                return CGPoint(x: 0.5, y: 0.5)
+            case .bottom:
+                return CGPoint(x: 0.5, y: 1)
+            case .topTrailing:
+                return CGPoint(x: 1, y: 0)
+            case .trailing:
+                return CGPoint(x: 1, y: 0.5)
+            case .bottomTrailing:
+                return CGPoint(x: 1, y: 1)
+            }
+        }
+    }
 }
